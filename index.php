94c5d55a8af86e495ef0c27335f2e9af2abd35de
Janu Ayodya <wmajanodani@students.nsbm.ac.lk>
2023-10-16 08:26:39 +0530

    registration

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel = "stylesheet" href = "CSS/index.css">
+  <title>Document</title>
+</head>
+<body>
+  
+      <div id="container">
+         <header>Registration</header>
+         <form method="post">
+            <fieldset>
+               <br/>
+               <input type="text" name="name" id="name" placeholder="Name" required autofocus >
+               <br/><br/>
+               <input type="text" name="stuid" id="stuid" placeholder="Student ID" required  >
+               <br/><br/>
+               <input type="text" name="stbatchid" id="batch" placeholder="Batch" required  >
+               <br/><br/>
+               <input type="text" name="degree" id="degree" placeholder="Degree" required  >
+               <br/><br/>
+        
+        
+           
+               
+
+                <input type="text" name="phone" id="phone" placeholder="Phone Number" required>
+                <br/><br/>
+               
+               
+               <input type="email" name="email" id="email" placeholder="E-mail" required>
+               <br/><br/>
+              
+               <label for="submit"></label>
+               <input type="submit" name="submit" id="submit" value="REGISTER">
+            </fieldset>
+         </form>
+      </div>
+   </body>
+
+
+
+</html>
