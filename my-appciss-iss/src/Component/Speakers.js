fb5b2e01c80a5c7171910ae2c48ebef86b64d6aa
Janu Ayodya <wmajanodani@students.nsbm.ac.lk>
2023-10-17 20:47:48 +0530

    card

--- my-appciss-iss/src/Component/Speakers.js
+++ my-appciss-iss/src/Component/Speakers.js
@@ -2,10 +2,25 @@ import React from 'react'
 import '../CSS/Speakers.css'
 import '../JS/spaker.js'
 
+import  Cardcomp  from "./card";
 export default function Speakers() {
   
   return (
-    <div id="root"></div>
+    <div>
+      <div className=' container container-flex  '>
+        <div className='card'>
+        <ul >
+      <li><Cardcomp/></li>
+      <li><Cardcomp/></li>
+      <li><Cardcomp/></li>
+      <li><Cardcomp/></li>
+      <li><Cardcomp/></li>
+      <li><Cardcomp/></li>
+          </ul>
+  
+    </div>
+    </div>
+    </div>
     
     
   )
---
+++ my-appciss-iss/src/Component/card.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import '../CSS/card.css'
+
+export default function card() {
+  return (
+    <>
+    <div class="box1 box">
+    <div class="content">
+      <div class="image">
+        {/* <img src="https://i.postimg.cc/bryMmCQB/profile-image.jpg" alt="Profile Image"> */}
+      </div>
+      <div class="level">
+        <p>PRO</p>
+      </div>
+      <div class="text">
+        <p class="name">Ethan Rivers</p>
+        <p class="job_title">UI / UX Designer</p>
+        <p class="job_discription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam atque, ipsam a amet laboriosam eligendi.</p>
+      </div>
+      <div class="icons">
+        <button>
+          <ion-icon name="logo-dribbble"></ion-icon>
+        </button>
+        <button>
+          <ion-icon name="logo-instagram"></ion-icon>
+        </button>
+        <button>
+          <ion-icon name="logo-twitter"></ion-icon>
+        </button>
+        <button>
+          <ion-icon name="logo-linkedin"></ion-icon>
+        </button>
+        <button>
+          <ion-icon name="logo-facebook"></ion-icon>
+        </button>
+        <button>
+          <ion-icon name="logo-behance"></ion-icon>
+        </button>
+      </div>
+      <div class="button">
+        <div>
+          <button class="message" type="button">Message</button>
+        </div>
+        <div>
+          <button class="connect" type="button">Connect</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+ 
+    </>
+
+  )
+}
